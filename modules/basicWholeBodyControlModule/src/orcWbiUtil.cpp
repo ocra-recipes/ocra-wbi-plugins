c2eecc41c4671ce4e1f3ba3c4efcba88555c0b62
Darwin Lau <lau@isir.upmc.fr>
2014-07-24 12:33:50 +0200

    Implemented row major to col major conversion in orcWbiUtil

--- modules/basicWholeBodyControlModule/src/orcWbiUtil.cpp
+++ modules/basicWholeBodyControlModule/src/orcWbiUtil.cpp
@@ -6,6 +6,8 @@
 * Utility functions for working with ORC/Eigen and Whole Body Interface
 */
 
+#include <iostream>
+
 #include "orcWbiUtil.h"
 
     /* static */ bool orcWbiConversions::eigenDispToWbiFrame(const Eigen::Displacementd &disp, wbi::Frame &frame)
@@ -20,7 +22,17 @@
 
     /* static */ bool orcWbiConversions::eigenRowMajorToColMajor(MatrixXdRm &M_rm, Eigen::MatrixXd &M)
     {
-        return false;
+        if((M_rm.cols() != M.cols()) || (M_rm.rows() != M.rows()))
+        {
+            std::cout<<"ERROR: Sizes of row major matrix and col major matrix are inconsistent"<<std::endl;
+            return false;
+        }
+
+        for(unsigned int i = 0; i < M_rm.rows(); i++)
+            for(unsigned int j = 0; j < M_rm.cols(); j++)
+                M(i,j) = M_rm(i,j);
+            
+        return true;
     }
 
 
