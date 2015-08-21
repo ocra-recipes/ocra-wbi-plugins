a367586d13f7505fdcbe2327d915104ccfa13361
Mingxing Liu <liu@isir.upmc.fr>
2015-08-21 15:58:39 -0400

    add jointLimitConstraint and torqueLimitConstraint

--- libs/taskSequences/include/taskSequences/sequences/FloatingBaseCoMBalancing.h
+++ libs/taskSequences/include/taskSequences/sequences/FloatingBaseCoMBalancing.h
@@ -2,6 +2,8 @@
 #define FLOATINGBASECOMBALANCING_H
 
 #include "wocra/Tasks/wOcraTaskSequenceBase.h"
+#include "wocra/Constraints/JointLimitConstraint.h"
+#include "wocra/Constraints/TorqueLimitConstraint.h"
 #include "../sequenceTools.h"
 
 #include <fstream>
@@ -18,13 +20,20 @@
             wocra::wOcraController*                        ctrl;
             wocra::wOcraModel*                             model;
             wocra::wOcraCoMTaskManager*                    tmCoM;
+
+            //Limits
+            wocra::JointLimitConstraint*                   jlConstraint;
+            Eigen::VectorXd                                torqueSaturationLimit; /* actuatedDOFs */
+            wocra::TorqueLimitConstraint*                  tauLimitConstraint;
+
             std::ofstream                                  datafile;
             std::vector<double>                            actual_com_y;
             std::vector<double>                            ref_com_y;
             bool                                           recorded;
             void sinusoidalTraj(double left, double right, double period, double t, double& posTraj, double& velTraj, double& accTraj);
             void saveCoMData();
-
+            void setJointTorqueLimits();
+            void setJointLimits(double hpos);
 
     };
 
--- libs/taskSequences/src/sequences/FloatingBaseCoMBalancing.cpp
+++ libs/taskSequences/src/sequences/FloatingBaseCoMBalancing.cpp
@@ -7,6 +7,9 @@
 
 
 #define PI 3.14159265
+#define TAU_MAX_LEG 12.0
+#define TAU_MAX_ARM 8.0
+#define TAU_MAX 24.0
 
 // namespace sequence{
     void FloatingBaseCoMBalancing::doInit(wocra::wOcraController& c, wocra::wOcraModel& m)
@@ -26,14 +29,14 @@
         w_full[model->getDofIndex("torso_roll")] = 1;
         w_full[model->getDofIndex("torso_yaw")] = 1;
 
-        taskManagers["fullPostureTask"] = new wocra::wOcraFullPostureTaskManager(*ctrl, *model, "fullPostureTask", ocra::FullState::INTERNAL, 10.0, 2*sqrt(10.0), w_full, q_full, usesYARP);
+        taskManagers["fullPostureTask"] = new wocra::wOcraFullPostureTaskManager(*ctrl, *model, "fullPostureTask", ocra::FullState::INTERNAL, 5.0, 2*sqrt(5.0), w_full, q_full, usesYARP);
 
 
 
         // Initialise com task
         Eigen::Vector3d desiredCoMPosition;
         desiredCoMPosition = model->getCoMPosition();
-        taskManagers["CoMTask"] = new wocra::wOcraCoMTaskManager(*ctrl, *model, "CoMTask", ocra::XY, 50.0, 2*sqrt(50.0), 10.0, desiredCoMPosition, usesYARP);
+        taskManagers["CoMTask"] = new wocra::wOcraCoMTaskManager(*ctrl, *model, "CoMTask", ocra::XY, 50.0, 1*sqrt(50.0), 10.0, desiredCoMPosition, usesYARP);
         tmCoM = dynamic_cast<wocra::wOcraCoMTaskManager*>(taskManagers["CoMTask"]);
 
 
@@ -63,6 +66,13 @@
         RFContacts.push_back(Eigen::Displacementd(Eigen::Vector3d( 0.06, 0.02,0.0), rotRZdown));
         taskManagers["rightFootContactTask"] = new wocra::wOcraContactSetTaskManager(*ctrl, *model, "rightFootContactTask", "r_sole", RFContacts, mu_sys, margin);
 
+        // Joint limits
+        jlConstraint = new wocra::JointLimitConstraint(*model, model->getJointLowerLimits(), model->getJointUpperLimits(), 0.1);
+        ctrl->addConstraint(*jlConstraint);
+
+        // Joint torque limits
+        setJointTorqueLimits();
+
 
     }
 
@@ -77,8 +87,8 @@
             double rf = model->getSegmentPosition(model->getSegmentIndex("r_foot")).getTranslation()[1];
 
 
-            double left = 0.8 * lf + 0.2 * rf;
-            double right = 0.8 * rf + 0.2 * lf;
+            double left = 0.9 * lf + 0.1 * rf;
+            double right = 0.9 * rf + 0.1 * lf;
             double com_y, v_com_y, a_com_y;
             sinusoidalTraj(left, right, period, time-ti, com_y, v_com_y, a_com_y);
 
@@ -126,5 +136,51 @@
 
         datafile.close();
         std::cout<<"data saved."<<std::endl;
+    }
+
+    void FloatingBaseCoMBalancing::setJointTorqueLimits()
+    {
+        torqueSaturationLimit = Eigen::VectorXd::Constant(model->nbInternalDofs(),TAU_MAX);
+        std::vector<std::string> legDofVector;
+        legDofVector.push_back("l_hip_pitch");
+        legDofVector.push_back("l_hip_roll");
+        legDofVector.push_back("l_hip_yaw");
+        legDofVector.push_back("l_knee");
+        legDofVector.push_back("l_ankle_pitch");
+        legDofVector.push_back("l_ankle_roll");
+        legDofVector.push_back("r_hip_pitch");
+        legDofVector.push_back("r_hip_roll");
+        legDofVector.push_back("r_hip_yaw");
+        legDofVector.push_back("r_knee");
+        legDofVector.push_back("r_ankle_pitch");
+        legDofVector.push_back("r_ankle_roll");
+        for (std::vector<std::string>::iterator it = legDofVector.begin() ; it != legDofVector.end(); ++it)
+            torqueSaturationLimit[model->getDofIndex(*it)] = TAU_MAX_LEG;
+        std::vector<std::string> armDofVector;
+        armDofVector.push_back("l_shoulder_pitch");
+        armDofVector.push_back("l_shoulder_roll");
+        armDofVector.push_back("l_shoulder_yaw");
+        armDofVector.push_back("l_elbow");
+        armDofVector.push_back("l_wrist_prosup");
+        armDofVector.push_back("l_wrist_pitch");
+        armDofVector.push_back("l_wrist_yaw");
+        armDofVector.push_back("r_shoulder_pitch");
+        armDofVector.push_back("r_shoulder_roll");
+        armDofVector.push_back("r_shoulder_yaw");
+        armDofVector.push_back("r_elbow");
+        armDofVector.push_back("r_wrist_prosup");
+        armDofVector.push_back("r_wrist_pitch");
+        armDofVector.push_back("r_wrist_yaw");
+        for (std::vector<std::string>::iterator it = armDofVector.begin() ; it != armDofVector.end(); ++it)
+            torqueSaturationLimit[model->getDofIndex(*it)] = TAU_MAX_ARM;
+
+        tauLimitConstraint = new wocra::TorqueLimitConstraint(*model,torqueSaturationLimit);
+        ctrl->addConstraint(*tauLimitConstraint);
+    }
+
+    void FloatingBaseCoMBalancing::setJointLimits(double hpos)
+    {
+        jlConstraint = new wocra::JointLimitConstraint(*model, model->getJointLowerLimits(), model->getJointUpperLimits(), hpos);
+        ctrl->addConstraint(*jlConstraint);
     }
 // }
