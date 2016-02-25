c9627f9edffdb4ef925b8ea99153ba7bfdf43b69
Ryan Lober <rlober@gmail.com>
2016-02-25 15:09:24 +0100

    added file for message vocab

---
+++ libs/ocra-yarp/include/ocra-yarp/OcraYarpVocab.h
@@ -0,0 +1,15 @@
+#ifndef OCRA_YARP_VOCAB_H
+#define OCRA_YARP_VOCAB_H
+
+#include <iostream>
+
+namespace ocra_yarp
+{
+
+enum OCRA_YARP_MESSAGE_TAG
+{
+    TEST
+};
+
+} // namespace ocra_yarp
+#endif // OCRA_YARP_VOCAB_H
