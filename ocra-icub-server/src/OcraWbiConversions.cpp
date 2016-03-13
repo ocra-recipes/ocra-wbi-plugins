c1c4a1e688a052df991272e3bf591b7aa3213204
Ryan Lober <rlober@gmail.com>
2016-03-13 14:43:48 +0100

    Fix conversion methods - this was what prevent free floating from working.

--- ocra-icub-server/src/OcraWbiConversions.cpp
+++ ocra-icub-server/src/OcraWbiConversions.cpp
@@ -30,54 +30,30 @@
 /* static */ bool OcraWbiConversions::eigenDispdToWbiFrame(const Eigen::Displacementd &disp, wbi::Frame &frame)
     {
 		double p_wbi[3];
-		Eigen::Vector3d p;
-		p = disp.getTranslation();
 
-		p_wbi[0] = p[0];
-		p_wbi[1] = p[1];
-		p_wbi[2] = p[2];
+		p_wbi[0] = disp.x();
+		p_wbi[1] = disp.y();
+		p_wbi[2] = disp.z();
 
-		double _x, _y, _z, _w;
-		_x = disp.getRotation().x();
-		_y = disp.getRotation().y();
-		_z = disp.getRotation().z();
-		_w = disp.getRotation().w();
+        wbi::Rotation R = wbi::Rotation::quaternion(disp.qx(), disp.qy(), disp.qz(), disp.qw());
 
-		wbi::Rotation R;	R.quaternion(_x, _y, _z, _w);
-		wbi::Frame _frame(R, p_wbi);
-		frame = _frame;
-
-		// Check if y-translation value is the same between the two data containers
-		if (frame.p[1]!=p(1,0)){
-			std::cerr << "Displacementd could not be converted to Wbi Frame \n";
-			return false;
-		}
+		frame = wbi::Frame(R, p_wbi);
 
 		return true;
     }
 
 /* static */ bool OcraWbiConversions::wbiFrameToEigenDispd(const wbi::Frame &frame, Eigen::Displacementd &disp)
     {
-		double _x, _y, _z, _w;
-		frame.R.getQuaternion(_x, _y, _z, _w);
+		double qx, qy, qz, qw;
+		frame.R.getQuaternion(qx, qy, qz, qw);
 
-		double x, y, z;
-		x = frame.p[0];
-		y = frame.p[1];
-		z = frame.p[2];
-
-		Eigen::Vector3d trans;
-		trans << x, y, z;
-
-		Eigen::Displacementd _disp(x,y,z,_w,_x,_y,_z);
-
-		disp = _disp;
-
-		// Check if y-translation value is the same between the two data containers
-		if (disp.getTranslation()(1,0)!=y){
-			std::cerr << "Wbi Frame could not be converted to Displacementd\n";
-			return false;
-		}
+		disp  = Eigen::Displacementd( frame.p[0],
+                                        frame.p[1],
+                                        frame.p[2],
+                                        qw,
+                                        qx,
+                                        qy,
+                                        qz);
 
 		return true;
     }
