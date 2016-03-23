dfb50e4874883a0730fbe0a856a499af53b1ba18
Ryan Lober <rlober@gmail.com>
2016-03-23 16:40:26 +0100

    Useful header.

---
+++ ocra-icub/include/ocra-icub/IcubClient.h
@@ -0,0 +1,11 @@
+#ifndef ICUB_CLIENT
+#define ICUB_CLIENT
+
+#include <ocra-icub/IcubControllerClient.h>
+#include <ocra-icub/IcubControllerClientManager.h>
+#include <ocra-icub/ModelInitializer.h>
+#include <ocra-icub/OcraWbiConversions.h>
+#include <ocra-icub/OcraWbiModel.h>
+#include <ocra-icub/Utilities.h>
+
+#endif
