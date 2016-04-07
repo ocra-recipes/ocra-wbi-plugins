681f656c607470ec76ad14862393ea911cb1c1dd
Ryan Lober <rlober@gmail.com>
2016-04-08 01:40:09 +0200

    Updates to reflect changes in message vocab

--- ocra-icub/include/ocra-icub/IcubClient.h
+++ ocra-icub/include/ocra-icub/IcubClient.h
@@ -1,8 +1,6 @@
 #ifndef ICUB_CLIENT
 #define ICUB_CLIENT
 
-#include <ocra-icub/IcubControllerClient.h>
-#include <ocra-icub/IcubControllerClientManager.h>
 #include <ocra-icub/ModelInitializer.h>
 #include <ocra-icub/OcraWbiConversions.h>
 #include <ocra-icub/OcraWbiModel.h>
