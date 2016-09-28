28c071aa8cac14edec9d5c78b13dff0c7722ea7a
Jorhabib <jorhabib@gmail.com>
2016-09-28 17:52:22 +0200

    Documented the stepping-demo client

--- ocra-icub-clients/stepping-demo/include/stepping-demo/SteppingDemoClient.h
+++ ocra-icub-clients/stepping-demo/include/stepping-demo/SteppingDemoClient.h
@@ -37,9 +37,9 @@ public:
 
 protected:
     /**
-     This methods does the following:
-     1. Define trajectory types (e.g. Min. Jerk)
-     2. Define the termination strategy
+     The initialization consists of the following:
+     1. Defines trajectory types (e.g. Min. Jerk)
+     2. Defines the termination strategy
      3. Instantiates trajectory threads for the left and right foot as well as for the CoM, sets them up (max velocity, error threshold) and finally starts them.
      4. Instantiates ocra::TaskConnection objects for the left and right feet contacts (4 per foot on each corner).
      
@@ -89,22 +89,79 @@ private:
      */
     Eigen::Vector3d getCoMPosition();
     
+    /**
+     *  Sets COM trajectory waypoints according to the desired support position:
+     *  LEFT_FOOT_XY: The COM goal position is set to be on top of the left foot.
+     *  RIGHT_FOOT_XY: The COM goal position is set to be on top of the right foot.
+     *  CENTERED_BETWEEN_FEET_XY: The COM goal position is set between both feet.
+     *  The height of the com is always the current COM's height. 
+     *  Since the thread must have started before calling this method, it will update the current waypoint trajectory it has or start it.
+     *
+     *  @param newSupportPos New support position.
+     */
     void positionCoMOver(COM_SUPPORT_POSITION newSupportPos);
 
+    /**
+     *  Uses the norm of the COM and joint velocities to understand if the robot is balanced.
+     *
+     *  @return True when both the norm of the COM and joint velocities are below comVelThreshold and jointThreshold.
+     *  @todo A more balance-centered criterion should be used for this, ZMP-based for example.
+     */
     bool isBalanced();
     
+    /**
+     *  Remember the right and left feet contact tasks? created in initialize(). This method acts the ocra-icub-server to deactivate one by one the feet "corner" contact tasks for the specified foot.
+     *
+     *  @param foot LEFT_FOOT or RIGHT_FOOT.
+     */
     void deactivateFootContacts(FOOT_CONTACTS foot);
     
+    /**
+     *  This method asks the ocra-icub-server to activate one by one the feet "corner" contact tasks for the specified foot.
+     *
+     *  @param foot LEFT_FOOT or RIGHT_FOOT.
+     */
     void activateFootContacts(FOOT_CONTACTS foot);
 
+    /**
+     *  If the foot height is less than or equal to footContactReleaseThreshold, the feet contacts tasks are activated.
+     *
+     *  @param foot LEFT_FOOT or RIGHT_FOOT.
+     *
+     *  @return True if the activation of the contact happens successfully. False if the foot's elevation is not under the footContactReleaseThreshold.
+     */
     bool isFootInContact(FOOT_CONTACTS foot);
 
+    /**
+     *  Mainly sets isPausing to true and stores the time at which the pause was called.
+     *
+     *  @param _pauseDuration Pause duration.
+     */
     void pauseFor(double _pauseDuration);
     
+    /**
+     *  Sets isPausing to false when pauseDuration has passed.
+     *
+     *  @return True when the pause duration is over. False otherwise.
+     */
     bool pauseFinished();
 
+    /**
+     *  Sets the right or left foot waypoint to rightFootTarget or leftFootTarget accordingly (where the foot will be lifted). These values are harcoded in the loop() method and updated only during the very first iteration when the variable getInitialValues is set to true.
+     *
+     *  @param foot LEFT_FOOT or RIGHT_FOOT
+     *
+     *  @return True after the foot contact is deactivated.
+     */
     bool liftFoot(FOOT_CONTACTS foot);
     
+    /**
+     *  Sets the right or left foot waypoint to rightFootHome or leftFootHome accordingly (where the foot will land). These values are harcoded in the loop() method and updated only during the very first iteration when the variable getInitialValues is set to true.
+     *
+     *  @param foot LEFT_FOOT or RIGHT_FOOT
+     *
+     *  @return True only after the foot has established contact again and the trajectory is over. False while the foot is still moving back down looking for contact or simply, the goal hasn't been attained. 
+     */
     bool setDownFoot(FOOT_CONTACTS foot);
 
     double pauseTriggerTime;
