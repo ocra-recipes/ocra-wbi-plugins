be6b15540415bc93708a5ff52bf00d8c6fbdd463
Ryan Lober <rlober@gmail.com>
2016-10-28 14:37:14 +0200

    Remove delay on startup.

--- ocra-icub-server/src/Thread.cpp
+++ ocra-icub-server/src/Thread.cpp
@@ -3,7 +3,7 @@
  *  \details
  *  \author     [Ryan Lober](http://www.ryanlober.com)
  *  \author     [Antoine Hoarau](http://ahoarau.github.io)
- *  \date       Feb 2016 
+ *  \date       Feb 2016
  *  \copyright  GNU General Public License.
  */
 /*
@@ -155,9 +155,9 @@ bool Thread::threadInit()
         yarpWbi->setControlReference(initialPosture.data());
         return yarpWbi->setControlMode(wbi::CTRL_MODE_TORQUE, 0, debugJointIndex);
     } else {
-        yarp::os::Time timer;
-        timer.delay(5.0);
-        std::cout << "First timer over" << std::endl;
+        // yarp::os::Time timer;
+        // timer.delay(5.0);
+        // std::cout << "First timer over" << std::endl;
         return yarpWbi->setControlMode(wbi::CTRL_MODE_TORQUE, 0, ALL_JOINTS);
 
         // jorh: This is mostly for when I'm debugging. Read current torques and set references, otherwise the robot will just fall under the action of zero torques being set when the control mode is set.
