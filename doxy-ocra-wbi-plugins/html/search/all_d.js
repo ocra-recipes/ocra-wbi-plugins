f9db50792b3a71f3dcebd37f46d092a18d1b5ff0
Jorhabib <jorhabib@gmail.com>
2016-10-30 02:03:35 +0100

    updated documentation

--- doxy-ocra-wbi-plugins/html/search/all_d.js
+++ doxy-ocra-wbi-plugins/html/search/all_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['ocra_2dwbi_2dplugins_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2focra_2drecipes_2focra_2dwbi_2dplugins_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2focra_2drecipes_2focra_2dwbi_2dplugins_29',['ocra-wbi-plugins [![Build Status](https://travis-ci.org/ocra-recipes/ocra-wbi-plugins.svg?branch=master)](https://travis-ci.org/ocra-recipes/ocra-wbi-plugins)',['../index.html',1,'']]],
+  ['ocra_2dwbi_2dplugins',['ocra-wbi-plugins',['../index.html',1,'']]],
   ['ocra_5ficub',['ocra_icub',['../namespaceocra__icub.html',1,'']]],
   ['ocra_5ficub_5fmessage',['OCRA_ICUB_MESSAGE',['../namespaceocra__icub.html#afbd2db66b68005fb7cfac19210caf83f',1,'ocra_icub']]],
   ['ocracontrolleroptions',['OcraControllerOptions',['../classOcraControllerOptions.html',1,'OcraControllerOptions'],['../classOcraControllerOptions.html#a1a91de992c42c6da488e95cd594eca80',1,'OcraControllerOptions::OcraControllerOptions()']]],
--- doxy-ocra-wbi-plugins/html/search/pages_0.js
+++ doxy-ocra-wbi-plugins/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['ocra_2dwbi_2dplugins_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2focra_2drecipes_2focra_2dwbi_2dplugins_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2focra_2drecipes_2focra_2dwbi_2dplugins_29',['ocra-wbi-plugins [![Build Status](https://travis-ci.org/ocra-recipes/ocra-wbi-plugins.svg?branch=master)](https://travis-ci.org/ocra-recipes/ocra-wbi-plugins)',['../index.html',1,'']]]
+  ['ocra_2dwbi_2dplugins',['ocra-wbi-plugins',['../index.html',1,'']]]
 ];
