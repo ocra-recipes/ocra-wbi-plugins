23514b60e66e29c396cb0162ccf8aa0c39d77287
Jorhabib <jorhabib@gmail.com>
2016-11-25 12:38:27 +0100

    Started walking-client
    Initial barebone commit

---
+++ ocra-icub-clients/walking-client/include/walking-client/WalkingClient.h
@@ -0,0 +1,27 @@
+#ifndef WALKINGCLIENT_H
+#define WALKINGCLIENT_H
+
+#include <ocra-icub/IcubClient.h>
+#include <ocra-recipes/TrajectoryThread.h>
+#include <ocra-recipes/ControllerClient.h>
+
+
+class WalkingClient : public ocra_recipes::ControllerClient
+{
+DEFINE_CLASS_POINTER_TYPEDEFS(WalkingClient)
+
+public:
+    WalkingClient (std::shared_ptr<ocra::Model> modelPtr, const int loopPeriod);
+    virtual ~WalkingClient ();
+
+protected:
+    virtual bool initialize();
+    virtual void release();
+    virtual void loop();
+
+private:
+
+};
+
+
+#endif // TEST_CLIENT_H
---
+++ ocra-icub-clients/walking-client/src/WalkingClient.cpp
@@ -0,0 +1,27 @@
+#include "walking-client/WalkingClient.h"
+WalkingClient::WalkingClient(std::shared_ptr<ocra::Model> modelPtr, const int loopPeriod)
+: ocra_recipes::ControllerClient(modelPtr, loopPeriod)
+{
+    // add your code here...
+}
+
+WalkingClient::~WalkingClient()
+{
+    // add your code here...
+}
+
+bool WalkingClient::initialize()
+{
+    // add your code here...
+    return true;
+}
+
+void WalkingClient::release()
+{
+    // add your code here...
+}
+
+void WalkingClient::loop()
+{
+    // add your code here...
+}
